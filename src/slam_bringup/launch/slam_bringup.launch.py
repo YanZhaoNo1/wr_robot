e228925575787b8fc707fb2dbb85f574bf19e66b
zy <YanZhaoNo1>
2025-04-15 15:33:36 +0800

    add slam_bringup

---
+++ src/slam_bringup/launch/slam_bringup.launch.py
@@ -0,0 +1,45 @@
+import launch
+import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription,ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    # 获取 rplidar_ros 和 wheeltec_n100_imu 的 launch 文件路径
+    rplidar_launch_file = os.path.join(
+        FindPackageShare("rplidar_ros").find("rplidar_ros"), "launch", "rplidar_launch.py"
+    )
+    imu_launch_file = os.path.join(
+        FindPackageShare("wheeltec_n100_imu").find("wheeltec_n100_imu"), "launch", "imu.launch.py"
+    )
+    static_tf_launch_file = os.path.join(
+        FindPackageShare("slam_bringup").find("slam_bringup"), "launch", "static_tf.launch.py"
+    )
+    lidar_undistortion_launch_file = os.path.join(
+        FindPackageShare("lidar_undistortion").find("lidar_undistortion"), "launch", "lidar_undistortion.launch.py"
+    )
+
+    return LaunchDescription([
+        # 启动 RPLidar
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rplidar_launch_file)
+        ),
+
+        # 启动 IMU
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(imu_launch_file)
+        ),
+
+        # 启动雷达去畸变
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(lidar_undistortion_launch_file)
+        ),
+        # 启动静态 TF 变换
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(static_tf_launch_file)
+        ),
+
+
+    ])
---
+++ src/slam_bringup/launch/static_tf.launch.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_odom',
+            arguments=['0', '0', '0', '0', '0', '0', '1','odom', 'base_link'],
+            output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='imu_to_base',
+            arguments=['0', '0', '0', '0', '0', '0','1','base_link', 'imu'],
+            output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='laser_to_base',
+            arguments=['-0.3954', '0', '0', '0', '0', '0', '1','base_link', 'laser'],
+            output='screen'),
+
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='laser_raw_to_base',
+        #     arguments=['-0.3954', '0', '0', '0', '0', '0','1', 'base_link', 'laser_raw'],
+        #     output='screen'),
+
+
+        # Node(
+        #     package='tf2_ros',
+        #     executable='static_transform_publisher',
+        #     name='rf2o_to_base',
+        #     arguments=['0', '-0.0954', '0', '0', '0', '0', 'odom_rf2o', 'base_link'],
+        #     output='screen'),
+
+    ])
+
