285c07c3d6d34079e8cd4d422567f30f90d0b952
zy <YanZhaoNo1>
2025-04-17 21:14:49 +0800

    Integrate sensor driver

---
+++ src/ucar_bringup/launch/static_tf_pub.launch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_odom',
+            arguments=['0', '0', '0', '0.924', '-0.383', '0', '0','map', 'odom'], # x 0.924 y -0383 
+            output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='static_tf_odom',
+            arguments=['0', '0', '0', '0', '0', '0', '1','odom', 'base_link'],
+            output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='imu_to_base',
+            arguments=['0', '0', '0', '0', '0', '0','1','base_link', 'imu'],
+            output='screen'),
+
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='laser_to_base',
+            arguments=['-0.3954', '0', '0', '0', '0', '0', '1','base_link', 'laser'],
+            output='screen'),
+    ])
+
---
+++ src/ucar_bringup/launch/ucar_driver.launch.py
@@ -0,0 +1,45 @@
+import launch
+import launch_ros.actions
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription,ExecuteProcess
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+import os
+
+def generate_launch_description():
+    rplidar_launch_file = os.path.join(
+        FindPackageShare("rplidar_ros").find("rplidar_ros"), "launch", "rplidar_launch.py"
+    )
+    imu_launch_file = os.path.join(
+        FindPackageShare("wheeltec_n100_imu").find("wheeltec_n100_imu"), "launch", "imu.launch.py"
+    )
+    wr_bot_launch_file = os.path.join(
+        FindPackageShare("turn_on_dlrobot_robot").find("turn_on_dlrobot_robot"), "launch", "tank.launch.py"
+    )
+    static_tf_launch_file = os.path.join(
+        FindPackageShare("ucar_bringup").find("ucar_bringup"), "launch", "static_tf_pub.launch.launch.py"
+    )
+
+    return LaunchDescription([
+        # 启动 RPLidar
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(rplidar_launch_file)
+        ),
+
+        # 启动 IMU
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(imu_launch_file)
+        ),
+
+        # 启动 机器人底盘
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(wr_bot_launch_file)
+        ),
+
+        # 启动静态 TF 变换
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(static_tf_launch_file)
+        ),
+
+
+    ])
